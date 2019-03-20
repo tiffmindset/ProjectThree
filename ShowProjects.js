91ea8b308dd7d31df326841de41b431c9254f551
Portfolio <catiffanymai@gmail.com>
2019-03-19 22:42:17 -0400

    complete project

--- ShowProjects.js
+++ ShowProjects.js
@@ -8,21 +8,21 @@ let curHr = date.getHours();
 let curMin = date.getMinutes();
 let curSec = date.getSeconds();
 
-let hrPosition = (curHr*360/12) + (curMin*(360/60)/12);
-let minPosition = (curMin*360/60) + (curSec*(360/60)/60);
-let secPosition = (curSec*360/60);
+let hrPosition = (curHr*(360/12)) + (curMin*(360/60)/12);
+let minPosition = (curMin*(360/60)) + (curSec*(360/60)/60);
+let secPosition = (curSec*(360/60));
 
 function runClock () {
-    
+
 hrPosition = hrPosition + (3/360);
 minPosition = minPosition + (6/60);
-secPosition = secPosition + 6;
+secPosition = secPosition + 6; 
 
-hrHand.style.transform = "rotate("+ hrPosition +"deg)";
-minHand.style.transform = "rotate("+ minPosition +"deg)";
-secHand.style.transform = "rotate("+ secPosition +"deg)";
+hrHand.style.transform = "rotate("+hrPosition+"deg)";
+minHand.style.transform = "rotate("+minPosition+"deg)";
+secHand.style.transform = "rotate("+secPosition+"deg)";
 
 }
 
-var interval = setInterval(runClock, 1000);
+ var interval = setInterval(runClock, 1000); 
 
